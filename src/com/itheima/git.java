1bd6f2061c1be404249e3ccec32ca6ac397cd635
mazengxiong <mzx123456>
2019-06-22 21:49:39 +0800

    提交

---
+++ src/com/itheima/git.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+public class git {
+    public static void main(String[] args) {
+
+        System.out.println("12");
+        System.out.println("12");
+        System.out.println("12");
+        System.out.println("12");
+        System.out.println("12");
+        System.out.println("12");
+    }
+}
